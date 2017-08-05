80a41613380aeeb966ec6e84061bd8b9556a82af
闻端Carl <wddwyss@gmail.com>
2017-08-05 18:51:14 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<div id="app">
+  <p>hello {{ who }}</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      who: 'monk studio'
+    }
+  }
+}
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+let path = require('path')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      { test: /\.vue$/, use: 'vue-loader' }
+    ]
+  }
+}
